fef25c0b32bda977d3a487b02693cf0137b7fb59
Munupeace <peacevwegbamunu@gmail.com>
2023-11-29 14:13:19 -0800

    python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(''.join(chr(value) for value in range(ord('a'), ord('z') + 1)), end='')
+for value in range(ord('a'), ord('z') + 1):
+    print(chr(value).format(), end='')
