1da3b0b2518448bf252841c6e6957508205d2eec
Munupeace <peacevwegbamunu@gmail.com>
2023-11-29 14:49:33 -0800

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for value in range(ord('a'), ord('z')):
+for value in range(ord('a'), ord('z') + 1):
     if value in (ord('q'), ord('e')):
         continue
     print("{}".format(chr(value)), end='')
